8b88c87d21ec0f815a625d9b1c2421ced3286a21
Keith Bauer <onesadcookie@gmail.com>
2011-11-26 18:33:50 -0800

    Initial Commit

---
+++ ORLY/ORAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ORAppDelegate.h
+//  ORLY
+//
+//  Created by Keith Bauer on 11/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ORAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ORLY/ORAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ORAppDelegate.m
+//  ORLY
+//
+//  Created by Keith Bauer on 11/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ORAppDelegate.h"
+
+@implementation ORAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
